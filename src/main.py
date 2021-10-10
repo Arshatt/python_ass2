d2b0fb2e558749b6e7fffc304e21dcb1010f16ea
Arshat <53335356+Arshatt@users.noreply.github.com>
2021-10-10 21:44:30 +0600

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+import lxml
+from selenium import webdriver
+
+
+class ParseArticle:
+    def collectInformation(self,nameOfCurrency):
+
+        URL='https://coinmarketcap.com/currencies/'+nameOfCurrency+'/news/'
+        r = requests.get(URL, 'html.parser').text
+        soup= BeautifulSoup(r,'lxml')
+        header=soup.h2.text
+        print(header,'\n')
+        wholeText=soup.text.strip()
+        count=0
+        for i in range(len(wholeText)):
+            print(wholeText[i],end='')
+            if wholeText[i]==' ':
+                count+=1
+            if count==6:
+                print('\n',end='')
+                count=0
+        print()
+        print('\n')
---
+++ test/test.py
@@ -0,0 +1,6 @@
+from main import ParseArticle
+
+prs=ParseArticle();
+
+x=input('Write currency name: ')
+prs.collectInformation(x)
